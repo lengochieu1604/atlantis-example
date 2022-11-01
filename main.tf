6d4ab509a286d3e5f301644728b60cefbd013763
hle-invidi <hle@invidi.com>
2022-11-01 09:43:41 +0700

    atlantis

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "null_resource" "example_00" {
 resource "null_resource" "example_11"{
 }
 
-resource "null_resource" "example_2"{
-}
+# resource "null_resource" "example_2"{
+# }
 
-resource "null_resource" "example_33"{
-}
+# resource "null_resource" "example_33"{
+# }
